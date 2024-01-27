ed3c83604034fdb93b1d1d86762592be839a2333
FarmerChillax <farmer-chong@qq.com>
2024-01-28 03:17:36 +0800

    chore: 移除无用代码

--- internal/xlog/log.go
+++ internal/xlog/log.go
@@ -16,11 +16,6 @@ func NewFormatter() *Formatter {
 }
 
 func (f *Formatter) Format(entry *logrus.Entry) ([]byte, error) {
-	// newLog := fmt.Sprintf("%s %s [%v,%v,%v](%s:%d)%s %s",
-	// 	timestamp, strings.ToUpper(entry.Level.String()),
-	// 	entry.Data["traceId"], entry.Data["spanId"], entry.Data["exportable"],
-	// 	path.Base(fileName), entry.Caller.Line, userFields, entry.Message)
-
 	span := trace.SpanFromContext(entry.Context)
 	entry.Data["trace_id"] = span.SpanContext().TraceID().String()
 	entry.Data["span_id"] = span.SpanContext().SpanID().String()
